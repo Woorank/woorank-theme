21aacba99b0bd555426fcc4d3a404f6dd3d89995
Jan Potoms <potoms.jan@gmail.com>
2014-11-21 18:10:52 +0100

    Initial commit with sc5-styleguide

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,53 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    s3 = require("gulp-s3"),
+    debug = require('gulp-debug'),
+    styleguide = require('sc5-styleguide');
 
-gulp.task('less', function () {
-  return gulp.src('./bootstrap.less')
-    .pipe(less())
+
+var sassOptions = {
+  includePaths: __dirname + '/node_modules/bootstrap-sass/assets/stylesheets/'
+};
+
+var styleguideOptions = {
+  title: 'My Styleguide',
+  overviewPath: './overview.md',
+  styleVariables: './lib/_variables.scss',
+  extraHead: [],
+  sass: sassOptions
+};
+
+gulp.task('build', function () {
+  return gulp.src('./lib/woorank-theme.scss')
+    .pipe(sass(sassOptions))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch', ['less'], function () {
-  return gulp.watch('./**/*.less', ['less']);
+gulp.task('develop', function() {
+  styleguideOptions.server = true;
+  styleguideOptions.rootPath = './styleguide/';
+  return gulp.src('./lib/**/*.scss')
+    .pipe(styleguide(styleguideOptions))
+    .pipe(gulp.dest(styleguideOptions.rootPath));
 });
 
-gulp.task('default', ['less']);
+gulp.task('build-styleguide', function() {
+  return gulp.src('./lib/**/*.scss')
+    .pipe(styleguide(styleguideOptions))
+    .pipe(gulp.dest('./styleguide'));
+});
+
+
+gulp.task('publish-styleguide', [ 'build-styleguide' ], function() {
+  var awsConfig = require('./awsConfig');
+  return gulp.src('./styleguide/**/*')
+    .pipe(s3(awsConfig));
+});
+
+gulp.task('watch-styleguide', ['build-styleguide'], function () {
+  return gulp.watch('./lib/**/*.scss', ['build-styleguide']);
+});
+
+gulp.task('default', ['build']);
---
+++ overview.md
@@ -0,0 +1,1 @@
+# WooRank theme styleguide
