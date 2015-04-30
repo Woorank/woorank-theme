3d4bcd3e3ebca325978a2b8bfe530dbeda553ecb
Didier Chartrain <didier@woorank.com>
2015-04-30 17:58:24 +0200

    update svg's & add js for genbar in header

--- src/js/woorank-theme.js
+++ src/js/woorank-theme.js
@@ -1,7 +1,9 @@
 var mobileNav = require('./modules/mobile-nav');
 var animations = require('./modules/animations');
 var tooltipCloser = require('./modules/tooltip-closer');
+var genbarOptions = require('./modules/generation-bar');
 
 mobileNav();
 animations();
 tooltipCloser();
+genbarOptions();
