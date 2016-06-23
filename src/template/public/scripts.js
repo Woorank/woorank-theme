1b2ad0d1ec5dda9fb1cae43a1bf5fc5bf5b2ba17
Didier Chartrain <didier@woorank.com>
2016-06-23 15:38:25 +0200

    refactor async button & add loader spin animation

--- src/template/public/scripts.js
+++ src/template/public/scripts.js
@@ -43,4 +43,56 @@
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
 
+  var btnAsync = {
+    default: 'btn btn-async ',
+    success: 'btn-async-success',
+    error: 'btn-async-error',
+    loader: 'btn-async-loading'
+  };
+
+  var btnColors = [
+    'btn-default',
+    'btn-primary',
+    'btn-warning',
+    'btn-success',
+    'btn-error',
+    'btn-link'
+  ];
+
+  var btnSizes = [
+    'btn-xs',
+    'btn-sm',
+    'btn-lg'
+  ];
+
+  $('[data-demo="js-btn-async-demo"]').each(function () {
+    var classes = $(this).attr('class').split(' ');
+    for (var c in classes) {
+      if (btnColors.indexOf(classes[c]) !== -1) {
+        $(this).attr('data-color', classes[c]);
+      }
+      if (btnSizes.indexOf(classes[c]) !== -1) {
+        $(this).attr('data-size', classes[c]);
+      }
+    }
+  });
+
+  $('[data-demo="js-btn-async-demo"]').on('click', function (event) {
+    var $btn = $(this);
+    var btnColor = $(this).attr('data-color') + ' ';
+    var btnSize = $(this).attr('data-size') + ' ';
+    var baseClasses = btnAsync.default + btnColor + btnSize;
+    event.preventDefault();
+
+    if ($btn.hasClass(btnAsync.error)) {
+      return $btn.attr('class', baseClasses);
+    }
+    if ($btn.hasClass(btnAsync.loader)) {
+      return $btn.attr('class', baseClasses + btnAsync.success);
+    }
+    if ($btn.hasClass(btnAsync.success)) {
+      return $btn.attr('class', baseClasses + btnAsync.error);
+    }
+    return $btn.attr('class', baseClasses + btnAsync.loader);
+  });
 })();
