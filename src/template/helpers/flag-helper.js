3b581ea70b79ca143801de20ac089e996e271356
Manuel Reyes <yoggsoft@gmail.com>
2016-08-19 10:33:15 +0200

    Update flag-helper.js
      'flag-uy': 'Venezuela', into   'flag-ve': 'Venezuela',

--- src/template/helpers/flag-helper.js
+++ src/template/helpers/flag-helper.js
@@ -59,7 +59,7 @@ var countries = {
   'flag-uk': 'United Kingdom',
   'flag-us': 'United States',
   'flag-uy': 'Uruguay',
-  'flag-uy': 'Venezuela',
+  'flag-ve': 'Venezuela',
   'flag-vn': 'Vietnam',
   'flag-za': 'South Africa'
 };
