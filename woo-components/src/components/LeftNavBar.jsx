40f304df92f11a1cf94e80b68b79745a604b3092
Markus Klinga <laite@gmx.com>
2016-09-05 17:32:49 +0200

    Add icon (#129)
    * Add <Icon /> to styleguide
    
    * name woo-icon
    
    * v7.2.0

--- woo-components/src/components/LeftNavBar.jsx
+++ woo-components/src/components/LeftNavBar.jsx
@@ -1,21 +1,27 @@
 const React = require('react');
 const classnames = require('classnames');
 
+const Icon = require('./Icon');
+
 class LeftNavBar extends React.Component {
   render () {
     const links = this.props.links.map((item, index) => {
       const active = item.active ? 'active' : '';
       const classes = classnames('woo-left-nav-link', active);
+
       return (
         <a className={classes}
           key={index}
           href={item.href || '#'}
-          onClick={item.action}>{item.label}</a>
+          onClick={item.action}>
+            <Icon type={item.icon} size='normal' />
+            {item.label}
+          </a>
       );
     });
 
     return (
-      <nav className='woo-left-nav'>
+      <nav className='woo-left-nav-bar'>
         {links}
       </nav>
     );
