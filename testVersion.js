5b5bd2b9adb420672d5f34b8d85842c8235d780c
Markus Klinga <markus@woorank.com>
2016-09-06 13:19:52 +0200

    Remove debug version number

--- testVersion.js
+++ testVersion.js
@@ -5,7 +5,7 @@ var http = require('http');
 module.exports = function (version, callback) {
   http.get({
     host: process.env.CDN_HOST,
-    path: '/woorank-theme/6.6.2/woorank-theme.min.css'
+    path: '/woorank-theme/' + version + '/woorank-theme.min.css'
   }, function (response) {
     var versionAlreadyExists = (response.statusCode === 200);
     callback(versionAlreadyExists);
