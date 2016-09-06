b6fb37478843a2cd6400638801a3e7203b9f9c77
Markus Klinga <markus@woorank.com>
2016-09-06 12:52:19 +0200

    Add version existance check on s3 task

--- deploy.js
+++ deploy.js
@@ -1,41 +1,27 @@
 #!/usr/bin/env node
 
-// Deploy the styleguide built files to aws on circleCI process.
-
 var exec = require('child_process').exec;
-var http = require('http');
+var testVersion = require('./testIfVersionExists');
 
-var theVersionAlreadyExists = false;
-
-// We get the package version
 var pkg = require('./package');
 var deployingVersion = pkg.version;
 
-// We define the deploy command on s3
-var deployCmd = 'aws s3 sync --acl public-read ' +
-  './styleguide/build ' + 's3://' + process.env.BUCKET_NAME;
+var deployCmd = 'aws s3 sync --acl public-read ./styleguide/build s3://' + process.env.BUCKET_NAME;
 
-// We check if this version has already been uploaded on S3
-http.get({
-  host: process.env.CDN_HOST,
-  path: '/woorank-theme/' + deployingVersion + '/woorank-theme.min.css'
-}, function (response) {
-  // Update our variable depending on the status code
-  theVersionAlreadyExists = (response.statusCode === 200);
-
-  // If the version doesn't exist , let's upload it
-  if (!theVersionAlreadyExists) {
-    exec(deployCmd, function (error, stdOut) {
-      if (!error) {
+testVersion(deployingVersion, function (alreadyExists) {
+  if (!alreadyExists) {
+    exec(deployCmd, function (error) {
+      if (error) {
+        console.error('Upload failed');
+        console.error(error);
+        process.exit(1);
+      } else {
         console.log('Uploaded OK');
         process.exit(0);
-      } else {
-        console.error('Uploaded Failed');
-        process.exit(1);
       }
     });
   } else {
-    console.error('Version already exists on S3 - not uploading!');
+    console.log('Version already exists on S3, not uploading.');
     process.exit(0);
   }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -222,10 +222,20 @@ gulp.task('sprite:build', function () {
     .pipe(gulp.dest(path.join('./styleguide/build/', pkg.version)));
 });
 
-gulp.task('s3', function () {
-  var awsConfig = require('./awsConfig');
-  return gulp.src('./styleguide/**/*')
-    .pipe(s3(awsConfig));
+gulp.task('s3', function (callback) {
+  var testVersion = require('./testIfVersionExists');
+  var version = require('./package').version;
+  testVersion(version, function (exists) {
+    var awsConfig = require('./awsConfig');
+
+    if (exists) {
+      return callback()
+    }
+    
+    gulp.src('./styleguide/**/*')
+      .pipe(s3(awsConfig))
+      .on('end', callback);
+    });
 });
 
 gulp.task('publish', function (callback) {
