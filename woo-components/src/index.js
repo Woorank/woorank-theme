4bba4f605f3ec7687e21f3e362ccfa73e28221eb
Markus Klinga <markus@woorank.com>
2016-09-09 12:59:31 +0200

    Rename LeftNavBar into GlobalMenu

--- woo-components/src/index.js
+++ woo-components/src/index.js
@@ -1,7 +1,6 @@
 module.exports.Badge = require('./components/Badge');
+module.exports.GlobalMenu = require('./components/GlobalMenu');
 module.exports.HelpIcon = require('./components/HelpIcon');
 module.exports.Icon = require('./components/Icon');
 module.exports.IssueCounter = require('./components/IssueCounter');
 module.exports.Label = require('./components/Label');
-module.exports.LeftNavBar = require('./components/LeftNavBar');
-
