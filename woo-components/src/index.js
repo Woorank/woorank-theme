faef4bdecaf1ed3c7be9d94404342d55187f40b2
Markus Klinga <laite@gmx.com>
2016-09-15 10:31:29 +0200

    New components - HelpIcon and IssueCounter (#142)
    * Add .lvimrc to .gitignore
    
    * Add IssueCounter
    
    * Move HelpIcon as its own component
    
    * v7.5.0
    
    * Fix index.js for woo-components
    
    * v7.5.1

--- woo-components/src/index.js
+++ woo-components/src/index.js
@@ -1,2 +1,5 @@
+module.exports.HelpIcon = require('./components/HelpIcon');
+module.exports.Icon = require('./components/Icon');
+module.exports.IssueCounter = require('./components/IssueCounter');
 module.exports.LeftNavBar = require('./components/LeftNavBar');
 
