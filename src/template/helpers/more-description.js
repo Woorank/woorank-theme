3ed6ad40a976978c8a264fa72256abbd296b0454
Manuel Reyes <yoggsoft@gmail.com>
2016-09-23 13:19:17 +0200

    Feature/pagination pattern (#148)
    * Fix gitignore .DS_Store
    
    * Add pagination markup+css
    
    * Add pagination-lg, pagination-sm on the markup
    
    * Add clean scss output
    
    * v7.8.0
    
    * Fix leading zero added for decimal variables

--- src/template/helpers/more-description.js
+++ src/template/helpers/more-description.js
@@ -2,8 +2,10 @@ module.exports.register = function (handlebars) {
   handlebars.registerHelper('moreDesc', function (input, options) {
     if (!(!input || typeof input !== 'string')) {
       var paragraphs = input.match(/(<p>[\s\S]*?<\/p>)/g);
-      if (paragraphs.length > 1) {
-        return options.fn(this);
+      if (paragraphs) {
+        if (paragraphs.length > 1) {
+          return options.fn(this);
+        }
       }
     }
   });
