024600e298e39f903a399dd8a29566ebb22b87d0
Markus Klinga <markus@woorank.com>
2016-10-06 11:45:26 +0200

    Remove IssueCounter (moved to serp-front)

--- woo-components/src/index.js
+++ woo-components/src/index.js
@@ -2,7 +2,6 @@ module.exports.Badge = require('./components/Badge');
 module.exports.ExpandableList = require('./components/ExpandableList');
 module.exports.HelpIcon = require('./components/HelpIcon');
 module.exports.Icon = require('./components/Icon');
-module.exports.IssueCounter = require('./components/IssueCounter');
 module.exports.Label = require('./components/Label');
 module.exports.LeftNavBar = require('./components/LeftNavBar');
 
