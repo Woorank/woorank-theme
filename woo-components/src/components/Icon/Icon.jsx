143f76753253a6720fd2f31f423e08a278ff2511
Markus Klinga <markus@woorank.com>
2016-11-02 17:56:19 +0100

    Allow custom asset path for icons

--- woo-components/src/components/Icon/Icon.jsx
+++ woo-components/src/components/Icon/Icon.jsx
@@ -2,7 +2,14 @@ const React = require('react');
 const classnames = require('classnames');
 const { PropTypes } = React;
 
+let assetPath = 'assets/symbols.svg';
+
 class Icon extends React.Component {
+
+  static setGlobalAssetPath (path) {
+    assetPath = path;
+  }
+
   render () {
     const { type, size, fill } = this.props;
     const extraClass = this.props.extraClass || false;
@@ -15,7 +22,7 @@ class Icon extends React.Component {
     return (
       <svg
         className={classnames('woo-icon icon', iconClasses, extraClass)}
-        dangerouslySetInnerHTML={{ __html: `<use xlink:href="assets/symbols.svg#${type}" />` }} />
+        dangerouslySetInnerHTML={{ __html: `<use xlink:href="${assetPath}#${type}" />` }} />
     );
   }
 }
