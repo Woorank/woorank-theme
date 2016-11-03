162739b95e10af1dfff0dd7c2863cc13880f7eb8
Markus Klinga <markus@woorank.com>
2016-11-03 13:01:04 +0100

    Rename variable

--- woo-components/config.js
+++ woo-components/config.js
@@ -1,6 +1,6 @@
 const pkg = require('./package.json');
 
 module.exports = {
-  iconPath: process.env.ASSET_PATH ||
+  iconPath: process.env.ICON_PATH ||
     `http://d384x5zk471rz.cloudfront.net/woorank-theme/${pkg.version}/symbols.svg`
 };
