5011b52cee1b1da5a1f2b127a92f1a71c60f4f74
Markus Klinga <laite@gmx.com>
2016-11-03 13:58:15 +0100

    Change the asset path (#169)

--- woo-components/config.js
+++ woo-components/config.js
@@ -1,6 +1,3 @@
-const pkg = require('./package.json');
-
 module.exports = {
-  iconPath: process.env.ICON_PATH ||
-    `http://d384x5zk471rz.cloudfront.net/woorank-theme/${pkg.version}/symbols.svg`
+  iconPath: process.env.ICON_PATH || '/en/serp/assets/svg/symbols.svg'
 };
