fd8fe0e258dc1b7d5505ddbe001274287f622175
Markus Klinga <markus@woorank.com>
2016-11-11 11:09:11 +0100

    Allow omitting background

--- woo-components/src/components/Label/Label.jsx
+++ woo-components/src/components/Label/Label.jsx
@@ -1,10 +1,16 @@
 const React = require('react');
 const { PropTypes } = React;
+
+const classnames = require('classnames');
 const { omit } = require('lodash');
 
 class Label extends React.Component {
   render () {
-    const classes = `woo-label woo-label-${this.props.style}`;
+    const classes = classnames(
+      'woo-label',
+      `woo-label-${this.props.style}`,
+      this.props.noBackground ? 'no-background' : ''
+    );
     return <span {...omit(this.props, 'style')} className={classes} />;
   }
 }
@@ -16,11 +22,13 @@ Label.propTypes = {
     'success',
     'warning',
     'danger'
-  ])
+  ]),
+  noBackground: PropTypes.bool
 };
 
 Label.defaultProps = {
-  style: 'default'
+  style: 'default',
+  noBackground: false
 };
 
 module.exports = Label;
--- woo-components/src/components/Label/README.md
+++ woo-components/src/components/Label/README.md
@@ -14,6 +14,16 @@ Colors:
       <Label style='danger'>Danger</Label>
     </div>
 
+Omitting background:
+
+    <div style={{ width: '250px', margin: 'auto' }}>
+      <Label noBackground>Default</Label>
+      <Label style='primary' noBackground>Primary</Label>
+      <Label style='success' noBackground>Success</Label>
+      <Label style='warning' noBackground>Warning</Label>
+      <Label style='danger' noBackground>Danger</Label>
+    </div>
+
 Additional properties:
 
     <div style={{ width: '250px', margin: 'auto' }}>
