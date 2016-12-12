d382353e5a0283283a1c209a5310e9b564749455
yoggsoft <yoggsoft@gmail.com>
2016-12-12 19:01:28 +0100

    Add right collapse header support

--- src/template/public/scripts.js
+++ src/template/public/scripts.js
@@ -13,25 +13,25 @@
       $(window).scrollTop(0);
     }
     $navbar.find('.navbar-collapse').collapse(status ? 'show' : 'hide');
-    $('body').toggleClass('navbar-open', status);
+    $('body').toggleClass('navbar-open-right', status);
   }
 
   $(document).on('click', function (event) {
     if (
       !$(event.target).parents('.navbar-header').length &&
       !$(event.target).parents('*[class^="js-"],*[class*=" js-"]').length &&
-      $('body').hasClass('navbar-open')
+      $('body').hasClass('navbar-open-right')
     ) {
       togglePanel(false);
     }
   });
 
   $navbar.find('.navbar-toggle').on('click', function (event) {
-    togglePanel(!$('body').hasClass('navbar-open'));
+    togglePanel(!$('body').hasClass('navbar-open-right'));
   });
 
   $(window).on('resize', function () {
-    if ($('body').hasClass('navbar-open')) {
+    if ($('body').hasClass('navbar-open-right')) {
       togglePanel(false);
     }
   });
