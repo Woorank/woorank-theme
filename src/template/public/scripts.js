925840f95de50e3529c92e0b6f187c951372216d
yoggsoft <yoggsoft@gmail.com>
2017-05-30 19:30:22 +0200

    Improve redundant stuff on navbar

--- src/template/public/scripts.js
+++ src/template/public/scripts.js
@@ -2,7 +2,7 @@
 
 (function () {
   var $ = window.jQuery;
-  var $navbar = $('#top .header.navbar');
+  var $navbar = $('#top .navbar');
   var $sidebarNav = $('.sidebar-nav');
 
   window.prettyPrint();
@@ -13,25 +13,25 @@
       $(window).scrollTop(0);
     }
     $navbar.find('.navbar-collapse').collapse(status ? 'show' : 'hide');
-    $('body').toggleClass('navbar-open-right', status);
+    $('body').toggleClass('navbar-open', status);
   }
 
   $(document).on('click', function (event) {
     if (
       !$(event.target).parents('.navbar-header').length &&
       !$(event.target).parents('*[class^="js-"],*[class*=" js-"]').length &&
-      $('body').hasClass('navbar-open-right')
+      $('body').hasClass('navbar-open')
     ) {
       togglePanel(false);
     }
   });
 
   $navbar.find('.navbar-toggle').on('click', function (event) {
-    togglePanel(!$('body').hasClass('navbar-open-right'));
+    togglePanel(!$('body').hasClass('navbar-open'));
   });
 
   $(window).on('resize', function () {
-    if ($('body').hasClass('navbar-open-right')) {
+    if ($('body').hasClass('navbar-open')) {
       togglePanel(false);
     }
   });
