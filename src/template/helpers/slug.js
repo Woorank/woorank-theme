2bae21ca3d4cca22aa431900c21cddfef3ad401b
Manuel Reyes <yoggsoft@gmail.com>
2019-02-06 11:17:54 +0100

    No flags (#265)
    * Remove Flag Icons
    
    * 10.8.0

--- src/template/helpers/slug.js
+++ src/template/helpers/slug.js
@@ -2,7 +2,7 @@ module.exports.register = function (handlebars) {
   handlebars.registerHelper('slug', function (input, options) {
     if (!input || typeof input !== 'string') return input;
     return new handlebars.SafeString(
-       input.replace(/^\s+|\s+$/g, '').toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-')
-     );
+      input.replace(/^\s+|\s+$/g, '').toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-')
+    );
   });
 };
