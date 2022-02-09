c3fe25454e0219d0c8341d4aa39fc133606a583d
Ali Muhammad <amuhammad@bridgeline.com>
2022-02-09 10:19:32 -0500

    update: task added that builds all icons to svg folder

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ const paths = {
     img: 'styleguide/assets/img',
     css: 'styleguide/assets/styles',
     svg: 'styleguide/assets/svg',
+    svgDump: 'styleguide/assets/svgDump',
     png: 'styleguide/assets/png',
     js: 'styleguide/assets/scripts'
   }
@@ -115,7 +116,7 @@ gulp.task('sass', function () {
     .pipe(debug())
     .pipe(sass({
       importer: moduleImporter({
-        extensions: [ '.css', '.scss' ]
+        extensions: ['.css', '.scss']
       }),
       imagePath: paths.build.img,
       outputStyle: 'expanded'
@@ -133,7 +134,7 @@ gulp.task('sass:build', function () {
     .pipe(debug())
     .pipe(sass({
       importer: moduleImporter({
-        extensions: [ '.css', '.scss' ]
+        extensions: ['.css', '.scss']
       }),
       imagePath: paths.build.img,
       outputStyle: 'compressed'
@@ -153,7 +154,7 @@ gulp.task('sass-kss', function () {
     .pipe(debug())
     .pipe(sass({
       importer: moduleImporter({
-        extensions: [ '.css', '.scss' ]
+        extensions: ['.css', '.scss']
       }),
       imagePath: paths.build.img,
       outputStyle: 'expanded'
@@ -174,6 +175,34 @@ gulp.task('svg:build', function () {
       }]
     }))
     .pipe(gulp.dest(path.join(paths.build.svg)));
+});
+gulp.task('svg:dump', function () {
+  let svgSrcPaths = [
+    '/01-basic',
+    '/02-arrows',
+    '/03-navigation',
+    '/04-network',
+    '/05-social',
+    '/06-technology',
+    '/07-application',
+    '/08-bullets',
+    '/09-logo',
+    '/10-contextuals',
+    '/11-third-party'
+  ];
+  svgSrcPaths.map(function (file) {
+    return gulp.src(path.join(paths.svg + file, '**', '*.svg'))
+      .pipe(svgmin({
+        plugins: [{
+          cleanupIDs: true,
+          removeUselessStrokeAndFill: true,
+          mergePaths: true,
+          removeUnknownsAndDefaults: false,
+          cleanupEnableBackground: true
+        }]
+      }))
+      .pipe(gulp.dest(path.join(paths.build.svg)));
+  })
 });
 
 gulp.task('svgstore', function () {
@@ -306,6 +335,7 @@ gulp.task('build', [
   'sass',
   'sass:build',
   'svg:build',
+  'svg:dump',
   'svgstore',
   'kss'
 ]);
